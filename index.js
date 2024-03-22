f587f2411efd9275fc29ce76536fa006d12656ef
spookeyy <pangasmeshack@gmail.com>
2024-03-22 17:22:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Hello!"
+let welcomeMessage = "Hello! Welcome to Flatbook, !"
+let excitedWelcomeMessage = "HELLO! WELCOME TO FLATBOOK, !"
+let shortGreeting = "H Welcome, !"
